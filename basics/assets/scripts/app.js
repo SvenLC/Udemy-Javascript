aeec2fc64703b009f9478731b162007871d02e02
SvenLC <sven.lecann@live.fr>
2019-12-08 23:39:15 +0100

    Section 2 - 25

--- basics/assets/scripts/app.js
+++ basics/assets/scripts/app.js
@@ -3,6 +3,7 @@ let currentResult = defaultResult;
 
 currentResult = ((currentResult + 10) * 3) / 2 - 1;
 
-let calculationDescription = '(' + defaultResult + '0 + 10) * 3 / 2 -1';
+let calculationDescription = `(${defaultResult} 0 + 10) * 3 / 2 -1`;
+let errorMessage = 'An error \n' + 'occured';
 
-outputResult(currentResult, calculationDescription);
+outputResult(currentResult, errorMessage);

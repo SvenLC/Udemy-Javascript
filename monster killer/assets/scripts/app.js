ca4123530f9b7030e8160c1b90151c4152a72893
SvenLC <sven.lecann@live.fr>
2019-12-18 22:21:39 +0100

    Section 4 - 100

--- monster killer/assets/scripts/app.js
+++ monster killer/assets/scripts/app.js
@@ -213,6 +213,15 @@ function printLogHandler() {
   // for (let i = 10; i > 0; i--) {
   //   console.log(i);
   // }
+  // for (let i = 0; i < battleLog.length; i++) {
+  //   console.log(battleLog[i]);
+  // }
+  let i = 0;
+  for (const logEntry of battleLog) {
+    console.log(logEntry);
+    console.log(i);
+    i++;
+  }
   console.log(battleLog);
 }
 

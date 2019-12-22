2cd5b52102a3ec4897673e0ba4501c5d8ed61f24
SvenLC <sven.lecann@live.fr>
2019-12-22 22:43:29 +0100

    Section 6 - 127

--- functions/app.js
+++ functions/app.js
@@ -4,13 +4,15 @@ function startGame() {
   console.log('Game is starting...');
 }
 
-const person = {
-  name: 'Max',
-  greet: function greet() {
-    console.log('Hello there!');
-  }
-};
+// const person = {
+//   name: 'Max',
+//   greet: function greet() {
+//     console.log('Hello there!');
+//   }
+// };
 
-person.greet();
+// person.greet();
+
+console.dir(startGame);
 
 startGameBtn.addEventListener('click', startGame);

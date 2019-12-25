0bcd834dc9371c3cfd900ab3641b43612667780a
SvenLC <sven.lecann@live.fr>
2019-12-25 21:20:13 +0100

    Section 6 - 140

--- functions/app.js
+++ functions/app.js
@@ -81,31 +81,61 @@ startGameBtn.addEventListener('click', () => {
 
 // not related to game
 
-const sumUp = (resultHandler, ...number) => {
+const combine = (resultHandler, operation, ...numbers) => {
   const validateNumber = number => {
     return isNaN(number) ? 0 : number;
   };
 
   let sum = 0;
   for (const num of numbers) {
-    sum += num;
+    if (operation === 'ADD') {
+      sum += validateNumber(num);
+    } else {
+      sum -= validateNumber(num);
+    }
   }
   resultHandler(sum);
 };
 
-const subtractUp = function() {
+const subtractUp = function(resultHandler, ...numbers) {
   let sum = 0;
-  for (const num of arguments) {
-    // don't use that
+  for (const num of numbers) {
     sum -= num;
   }
-  return sum;
+  resultHandler(sum);
 };
 
-const showResult = () => {
-  alert('The resut after adding all numbers is: ' + result);
+const showResult = (messageText, result) => {
+  alert(messageText + ' ' + result);
 };
 
-console.log(sumUp(showResult, 1, 5, 10 - 3, 6, 10));
-console.log(sumUp(1, 5, 10 - 3, 6, 10, 25, 88));
-console.log(subtractUp(1, 5, 10 - 3, 6, 10, 25, 88));
+combine(
+  showResult.bind(this, 'The result after adding all numbers is:'),
+  'ADD',
+  1,
+  5,
+  10 - 3,
+  6,
+  10
+);
+combine(
+  showResult.bind(this, 'The result after adding all numbers is:'),
+  'ADD',
+  1,
+  5,
+  10 - 3,
+  6,
+  10,
+  25,
+  88
+);
+combine(
+  showResult.bind(this, 'The result after subtracting all numbers is:'),
+  'SUBTRACT',
+  5,
+  10 - 3,
+  6,
+  10,
+  25,
+  88
+);

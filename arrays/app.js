5070ac3c2b592f711144fe518ebb0d3be130bdca
SvenLC <sven.lecann@live.fr>
2020-01-01 23:14:18 +0100

    Section 8 - 185

---
+++ arrays/app.js
@@ -0,0 +1,14 @@
+const numbers = [1, 2, 3];
+console.log(numbers);
+
+// moreNumbers = new Array(5);
+// console.log(moreNumbers);
+
+// const yetMoreNumbers = Array.of(1, 2);
+// console.log(yetMoreNumbers);
+
+const listItems = document.querySelector('li');
+console.log(listItems);
+
+const arrayListItems = Array.from(listItems);
+console.log(arrayListItems);

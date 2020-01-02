09b441e9e6dbd4892a815f284288cd78d621bf50
SvenLC <sven.lecann@live.fr>
2020-01-02 14:30:32 +0100

    Section 8 - 187

--- arrays/app.js
+++ arrays/app.js
@@ -1,30 +1,42 @@
-const numbers = [1, 2, 3];
-console.log(numbers);
+// const numbers = [1, 2, 3];
+// console.log(numbers);
 
-// moreNumbers = new Array(5);
-// console.log(moreNumbers);
+// // moreNumbers = new Array(5);
+// // console.log(moreNumbers);
 
-// const yetMoreNumbers = Array.of(1, 2);
-// console.log(yetMoreNumbers);
+// // const yetMoreNumbers = Array.of(1, 2);
+// // console.log(yetMoreNumbers);
 
-const listItems = document.querySelector('li');
-console.log(listItems);
+// const listItems = document.querySelector('li');
+// console.log(listItems);
 
-const arrayListItems = Array.from(listItems);
-console.log(arrayListItems);
+// const arrayListItems = Array.from(listItems);
+// console.log(arrayListItems);
 
-const hobies = ['Cooking', 'Sports'];
-const personnalData = [30, 'Max', { moreDetail: [] }];
+// const hobies = ['Cooking', 'Sports'];
+// const personnalData = [30, 'Max', { moreDetail: [] }];
 
-const analyticsData = [
-  [1, 1.6],
-  [-5.4, 2.1]
-];
+// const analyticsData = [
+//   [1, 1.6],
+//   [-5.4, 2.1]
+// ];
 
-for (const data of analyticsData) {
-  for (const dataPoint of data) {
-    console.log(dataPoint);
-  }
-}
+// for (const data of analyticsData) {
+//   for (const dataPoint of data) {
+//     console.log(dataPoint);
+//   }
+// }
 
-console.log(personnalData[1]);
+// console.log(personnalData[1]);
+
+const hobbies = ['Sports', 'Coocking'];
+
+hobbies.push('Reading');
+hobbies.unshift('Coding');
+const poppedValue = hobbies.pop();
+hobbies.shift();
+console.log(hobbies);
+
+hobbies[1] = 'Coding';
+hobbies[5] = 'Reading';
+console.log(hobbies);

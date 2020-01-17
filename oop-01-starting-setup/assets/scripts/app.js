cfb4100d0d1d8467da4de576e51746c45ce59700
SvenLC <sven.lecann@live.fr>
2020-01-17 20:49:24 +0100

    Section 10 - 245

--- oop-01-starting-setup/assets/scripts/app.js
+++ oop-01-starting-setup/assets/scripts/app.js
@@ -12,8 +12,31 @@ class Product {
   }
 }
 
-const productList = {
-  products: [
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
+
+  render() {
+    const prodEl = document.createElement('li');
+    prodEl.className = 'product-item';
+    prodEl.innerHTML = `
+        <div>
+          <img src="${this.product.imageUrl}" alt="${this.product.title}"/>
+          <div class="product-item__content">
+            <h2>${this.product.title}</h2>
+            <h3>\$${this.product.price}</h3>
+            <p>${this.product.description}</p>
+            <button>Add to Cart</button>
+          </div>
+        </div>
+        `;
+    return prodEl;
+  }
+}
+
+class ProductList {
+  products = [
     new Product(
       'A Pillow',
       'https://www.serta.com/sites/ssb/serta.com/uploads/2018/accessories/pillows/Cool%20Comfy%20Queen/CoolComfy1.jpg',
@@ -25,44 +48,24 @@ const productList = {
       'https://www.cdiscount.com/pdt2/9/0/0/1/550x550/sto7042421098900/rw/tapis-peluche-chambre-salon-carpet-moquette-d-enfa.jpg',
       'A carpet which you might like - or not.',
       89.99
-    ),
-    {
-      title: 'A Pillow',
-      imageUrl:
-        'https://www.serta.com/sites/ssb/serta.com/uploads/2018/accessories/pillows/Cool%20Comfy%20Queen/CoolComfy1.jpg',
-      price: 19.99,
-      description: 'A soft pillow!'
-    },
-    {
-      title: 'A Carpet',
-      imageUrl:
-        'https://www.cdiscount.com/pdt2/9/0/0/1/550x550/sto7042421098900/rw/tapis-peluche-chambre-salon-carpet-moquette-d-enfa.jpg',
-      price: 89.99,
-      description: 'A carpet which you might like - or not.'
-    }
-  ],
+    )
+  ];
+
+  constructor() {}
+
   render() {
     const renderHook = document.getElementById('app');
     const prodList = document.createElement('ul');
     prodList.className = 'product-list';
     for (const prod of this.products) {
-      const prodEl = document.createElement('li');
-      prodEl.className = 'product-item';
-      prodEl.innerHTML = `
-          <div>
-            <img src="${prod.imageUrl}" alt="${prod.title}"/>
-            <div class="product-item__content">
-              <h2>${prod.title}</h2>
-              <h3>\$${prod.price}</h3>
-              <p>${prod.description}</p>
-              <button>Add to Cart</button>
-            </div>
-          </div>
-          `;
+      const productItem = new ProductItem(prod);
+      const prodEl = productItem.render();
       prodList.append(prodEl);
     }
     renderHook.append(prodList);
   }
-};
+}
+
+const productList = new ProductList();
 
 productList.render();

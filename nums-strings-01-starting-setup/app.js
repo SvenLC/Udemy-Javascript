d687396951628d8d72f66af80e72115e6e88980d
SvenLC <sven.lecann@live.fr>
2020-03-08 22:18:33 +0100

    Section 16 - 343

--- nums-strings-01-starting-setup/app.js
+++ nums-strings-01-starting-setup/app.js
@@ -3,3 +3,22 @@ function randomIntBetween(min, max) {
 }
 
 console.lof(randomIntBetween(1, 10));
+
+function productDescription(strings, productName, productPrice) {
+  console.log(strings);
+  console.log(productName);
+  console.log(productPrice);
+  let priceCategory = 'cheap';
+  if (productPrice > 20) {
+    priceCategory = 'fair';
+  }
+  //   return `${string[0]}${productName}${string[1]}${priceCategory}${strings[2]}`;
+  return { name: productName, price: productPrice };
+}
+
+const prodName = 'Javascript Course';
+const prodPrice = 29.99;
+
+const productOutput = productDescription`This product (${prodName}) is ${prodPrice}.`;
+
+console.log(productOutput);

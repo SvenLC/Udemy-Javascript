6a47a6fd56966fb063361713d16c9e764bb9686b
Sven Le Cann <sven.lecann@live.fr>
2020-08-12 16:39:06 +0200

    Section 19 - 388

---
+++ lib-01-starting-setup/app.js
@@ -0,0 +1,7 @@
+const customers = ["Max", "Manuel", "Anna"];
+
+const activeCustomers = ["Max", "Anna"];
+
+const inactiveCustomers = _.difference(customers, activeCustomers);
+
+console.log(inactiveCustomers);

7ebc9f4ad6058173d19c9476142977937bf00cb6
Sven Le Cann <sven.lecann@live.fr>
2020-08-12 17:34:12 +0200

    Section 19 - 391

--- http-01-starting-setup/assets/scripts/app.js
+++ http-01-starting-setup/assets/scripts/app.js
@@ -50,11 +50,10 @@ function sendHttpRequest(method, url, data) {
 
 async function fetchPosts() {
   try {
-    const responseData = await sendHttpRequest(
-      "GET",
+    const response = await axios.get(
       "https://jsonplaceholder.typicode.com/posts"
     );
-    const listOfPosts = responseData;
+    const listOfPosts = response.data;
     for (const post of listOfPosts) {
       const postEl = document.importNode(postTemplate.content, true);
       postEl.querySelector("h2").textContent = post.title.toUpperCase();
@@ -80,7 +79,11 @@ async function createPost(title, content) {
   // fd.append("body", content);
   fd.append("userId", userId);
 
-  sendHttpRequest("POST", "https://jsonplaceholder.typicode.com/posts", post);
+  const response = await axios.post(
+    "https://jsonplaceholder.typicode.com/posts",
+    fd
+  );
+  console.log(response);
 }
 
 fetchButton.addEventListener("click", fetchPosts);
@@ -95,9 +98,6 @@ postList.addEventListener("click", (event) => {
   if (event.target.tagName === "BUTTON") {
     const postId = event.target.closest("li").id;
     console.log(postId);
-    sendHttpRequest(
-      "DELETE",
-      `https://jsonplaceholder.typicode.com/posts/${postId}`
-    );
+    axios.delete(`https://jsonplaceholder.typicode.com/posts/${postId}`);
   }
 });

cf5212e9a80dd3b499bd962959be09d20d6b40ca
Sven Le Cann <sven.lecann@live.fr>
2020-08-15 19:00:24 +0200

    Section 22 - 429

--- storage-01-starting-setup/app.js
+++ storage-01-starting-setup/app.js
@@ -3,8 +3,15 @@ const retrBtn = document.getElementById("retrieve-btn");
 
 storeBtn.addEventListener("click", () => {
   const userId = "u123";
-  document.cookie = `uid=${userId}`;
+  const user = { name: "Max", age: 30 };
+  document.cookie = `uid=${userId}; max-age=360`;
+  document.cookie = `user=${JSON.stringify(user)}`;
 });
 retrBtn.addEventListener("click", () => {
   console.log(document.cookie);
+  const cookieData = document.cookie.split(";");
+  const data = cookieData.map((i) => {
+    return i.trim();
+  });
+  console.log(data[1].split("=")[1]);
 });

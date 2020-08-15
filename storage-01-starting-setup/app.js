021d6148cb7751d6e9c62d5716edb9eef9836a89
Sven Le Cann <sven.lecann@live.fr>
2020-08-15 19:08:47 +0200

    Section 22 - 430

--- storage-01-starting-setup/app.js
+++ storage-01-starting-setup/app.js
@@ -1,17 +1,29 @@
 const storeBtn = document.getElementById("store-btn");
 const retrBtn = document.getElementById("retrieve-btn");
 
-storeBtn.addEventListener("click", () => {
-  const userId = "u123";
-  const user = { name: "Max", age: 30 };
-  document.cookie = `uid=${userId}; max-age=360`;
-  document.cookie = `user=${JSON.stringify(user)}`;
-});
-retrBtn.addEventListener("click", () => {
-  console.log(document.cookie);
-  const cookieData = document.cookie.split(";");
-  const data = cookieData.map((i) => {
-    return i.trim();
-  });
-  console.log(data[1].split("=")[1]);
-});
+const dbRequest = indexedDB.open("StorageDummy", 1);
+
+dbRequest.onupgradeneeded = function (event) {
+  const db = event.target.result;
+
+  const objStore = db.createObjectStore("products", { keyPath: "id" });
+
+  objStore.transaction.oncomplete = function (event) {
+    const productsStore = db
+      .transaction("products", "readwrite")
+      .objectStore("products");
+    productsStore.add({
+      id: "p1",
+      title: "A First Product",
+      price: 12.99,
+      tags: ["Expensive", "Luxury"],
+    });
+  };
+};
+
+dbRequest.onerror = function (event) {
+  console.log("ERROR!");
+};
+
+storeBtn.addEventListener("click", () => {});
+retrBtn.addEventListener("click", () => {});
